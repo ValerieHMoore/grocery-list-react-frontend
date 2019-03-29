0409cb78209dcfb9e528b293a3f013d57a59628e
ValerieHMoore <periwinkle17@outlook.com>
2019-03-29 16:23:42 -0400

    Add home component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Home from './components/Home'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        
-      </div>
+      <Router>
+        <div className="App">
+          <Route exact path='/' component={Home} />
+     
+        </div>
+      </Router>
     );
   }
 }
