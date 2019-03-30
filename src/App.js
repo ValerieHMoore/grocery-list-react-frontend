034f7e28c8ee5eb1a08a0f6dd84236678e8fd99f
ValerieHMoore <periwinkle17@outlook.com>
2019-03-30 18:03:17 -0400

    Add background image and change font color

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-// import './App.css';
+import groceryPhoto from './groceryPhoto.jpg';
+import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Home from './components/Home'
 import ItemsContainer from './containers/ItemsContainer'
