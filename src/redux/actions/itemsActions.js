87e2243f39ed15f477d30dc41f176c893ae6d689
ValerieHMoore <periwinkle17@outlook.com>
2019-03-30 13:28:29 -0400

    Rename fetch items

--- src/redux/actions/itemsActions.js
+++ src/redux/actions/itemsActions.js
@@ -2,6 +2,6 @@ export const fetchItems = () => dispatch => {
     return fetch("http://localhost:3001/items")
     .then(resp => resp.json())
     .then(items =>
-        dispatch({ type: 'FETCH_ITEMS', payload: items })
+        dispatch({ type: 'FETCH_ITEMS_SUCCESS', payload: items })
     )
 }
--- src/redux/reducers/itemsReducer.js
+++ src/redux/reducers/itemsReducer.js
@@ -2,7 +2,7 @@ const initialState = []
 
 export default (state = initialState, action) => {
     switch (action.type) {
-        case 'FETCH_ITEMS':
+        case 'FETCH_ITEMS_SUCCESS':
             return action.payload
         default:
             return state
