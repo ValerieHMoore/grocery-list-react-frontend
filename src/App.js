3757acee66d25db7ec30bb8bc93a62e580d843b7
ValerieHMoore <periwinkle17@outlook.com>
2019-03-30 14:05:43 -0400

    Add AddItemContainer

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Home from './components/Home'
 import ItemsContainer from './containers/ItemsContainer'
+import AddItemContainer from './containers/AddItemContainer'
 
 class App extends Component {
   render() {
---
+++ src/containers/AddItemContainer.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+class AddItemContainer extends Component {
+
+    state = {
+        name: '',
+        quantity: ''
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault()
+        this.props.addItem(this.state)
+        this.setState({
+          name: '',
+          quantity: ''
+        })
+    }
+    
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+        
+    render() {
+        return (
+            <AddItem input={this.state} handleSubmit={this.handleSubmit} handleChange={this.handleChange}/>
+        )}
+    }
+        
+        
+const mapDispatchToProps = dispatch => {
+    return {
+    addItem: (item) => dispatch(addItem(item))
+    }
+}
+        
+export default connect(null, mapDispatchToProps)(AddItemContainer)
