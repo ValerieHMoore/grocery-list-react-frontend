9c35c423ed4c4a065bbd619154007cbbbf8b64e5
ValerieHMoore <periwinkle17@outlook.com>
2019-03-29 20:32:16 -0400

    Finish items fetch action

--- src/redux/actions/itemsActions.js
+++ src/redux/actions/itemsActions.js
@@ -1,3 +1,7 @@
-export const fetchItems = () {
-    
+export const fetchItems = () => dispatch => {
+    return fetch("http://localhost:3001/items")
+    .then(resp => resp.json())
+    .then(items =>
+        dispatch({ type: 'FETCH_ITEMS', payload: items })
+    )
 }
