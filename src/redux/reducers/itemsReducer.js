c648a2b7a778244f42fb2ab716d88d586d6fd184
ValerieHMoore <periwinkle17@outlook.com>
2019-03-30 15:01:23 -0400

    Add item success to reducer

--- src/redux/reducers/itemsReducer.js
+++ src/redux/reducers/itemsReducer.js
@@ -4,6 +4,10 @@ export default (state = initialState, action) => {
     switch (action.type) {
         case 'FETCH_ITEMS_SUCCESS':
             return action.payload
+
+        case 'ADD_ITEM_SUCCESS':
+        return {...state, item: action.payload}
+        
         default:
             return state
     }
