cef8d7fef944cf68db8273d90241b16d1067e2e4
ValerieHMoore <periwinkle17@outlook.com>
2019-03-30 19:09:27 -0400

    Add item container

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Home from './components/Home'
 import ItemsContainer from './containers/ItemsContainer'
 import AddItemContainer from './containers/AddItemContainer'
+import ItemContainer from './containers/ItemContainer'
 
 class App extends Component {
   render() {
@@ -13,6 +14,7 @@ class App extends Component {
           <Route exact path='/' component={Home} />
           <Route exact path='/items' component={ItemsContainer} />
           <Route exact path="/items/new/item" component={AddItemContainer} />
+          <Route exact path='/items/:id' component={ItemContainer} />
         </div>
       </Router>
     );
