7564b193210cff2bb64ecb247d3134eb4d46c4a8
ValerieHMoore <periwinkle17@outlook.com>
2019-03-30 20:21:05 -0400

    Add fetch item reducer

--- src/redux/reducers/itemsReducer.js
+++ src/redux/reducers/itemsReducer.js
@@ -1,13 +1,19 @@
-const initialState = []
+const initialState = {
+    items: [],
+    item: {}
+}
 
 export default (state = initialState, action) => {
     switch (action.type) {
         case 'FETCH_ITEMS_SUCCESS':
-            return action.payload
-
-        case 'ADD_ITEM_SUCCESS':
-        return {...state, item: action.payload}
+            return {...state, items: action.payload}
         
+        case 'FETCH_ITEM_SUCCESS':
+            return {...state, item: action.payload}
+        
+        case 'ADD_ITEM_SUCCESS':
+            return {...state, item: action.payload}
+
         default:
             return state
     }
