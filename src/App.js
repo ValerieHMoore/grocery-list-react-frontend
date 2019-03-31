d034ba39815ebac91907d78f0b60dc5947130bd9
ValerieHMoore <periwinkle17@outlook.com>
2019-03-31 14:17:01 -0400

    Add Wrapper component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Home from './components/Home'
+import Wrapper from './components/Wrapper'
 import ItemsContainer from './containers/ItemsContainer'
 import AddItemContainer from './containers/AddItemContainer'
 import ItemContainer from './containers/ItemContainer'
@@ -9,14 +9,16 @@ import ItemContainer from './containers/ItemContainer'
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <Route exact path='/' component={Home} />
-          <Route exact path='/items' component={ItemsContainer} />
-          <Route exact path="/items/new/item" component={AddItemContainer} />
-          <Route exact path={`/items/:itemId`} component={ItemContainer} />
-        </div>
-      </Router>
+      <Wrapper>
+        <Router>
+          <div className="App">
+            <Route exact path='/' component={Home} />
+            <Route exact path='/items' component={ItemsContainer} />
+            <Route exact path="/items/new/item" component={AddItemContainer} />
+            <Route exact path={`/items/:itemId`} component={ItemContainer} />
+          </div>
+        </Router>
+      </Wrapper>
     );
   }
 }
---
+++ src/components/Wrapper.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+import img from '../shoppingCart.jpg';
+
+const Wrapper = styled.div`
+  background-image: url(${img});
+  background-size: cover;
+`;
+
+export default Wrapper
