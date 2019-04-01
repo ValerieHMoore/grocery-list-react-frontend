782490cbb6b16aafa8a1ba78903b740389648247
ValerieHMoore <periwinkle17@outlook.com>
2019-03-31 21:21:44 -0400

    Rename home page button

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,7 @@ export default () => {
     return (
         <div>
             <Header>Welcome to Grocery List</Header>
-            <Link to="/items"><Button type="button">See Items</Button></Link>
+            <Link to="/items"><Button type="button">See List Items</Button></Link>
         </div>
     )
 }
